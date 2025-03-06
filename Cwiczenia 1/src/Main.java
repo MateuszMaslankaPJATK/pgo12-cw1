c6a668a16b3c06471afb9505b082adc69b884759
MateuszMaslankaPJATK <s34431@pjwstk.edu.pl>
2025-03-06 09:41:44 +0100

    Change Main.java

--- Cwiczenia 1/src/Main.java
+++ Cwiczenia 1/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         String text = "Modyfikacja";
+
+        int a = 10;
+        int b = 123;
+        int c = 213;
+
     }
 }
